6ef68354d0066838e8f570e91ec53b7886a7264a
Etec 244 <e244ti@outlook.com>
2020-03-09 15:02:30 -0300

    projectAula

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import { CoreModule } from './core/core.module';
+import { CidadesModule } from './cidades/cidades.module';
+import { RouterModule, Routes } from '@angular/router';
+import { CidadesPesquisaComponent } from './cidades/cidades-pesquisa/cidades-pesquisa.component';
+
+const routes:Routes=[
+  {path:'cidades', component: CidadesPesquisaComponent}
+]
 
 @NgModule({
   declarations: [
@@ -10,7 +18,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot(routes),
+    AppRoutingModule,
+    CoreModule,
+    CidadesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
